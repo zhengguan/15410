f4a05cb0de5ac134112c7371ce44a1b6139811e0
Patrick Koenig <phkoenig@andrew.cmu.edu>
2015-03-04 17:36:34 -0500

    Fixed IDT init

--- p3/kern/idt.c
+++ p3/kern/idt.c
@@ -23,36 +23,36 @@ extern int remove_pages(void *base);
 void idt_init() {
 
     // /* Add timer and keyboard interrupt gate descriptors */
-    // idt_add_desc(TIMER_IDT_ENTRY, 0, IDT_INT);
-    // idt_add_desc(KEY_IDT_ENTRY, 0, IDT_INT);
+    // idt_add_desc(TIMER_IDT_ENTRY, 0, IDT_INT, IDT_DPL_KERNEL);
+    // idt_add_desc(KEY_IDT_ENTRY, 0, IDT_INT, IDT_DPL_KERNEL);
 
     // /* Add system call trap gate descriptors */
-    // idt_add_desc(SYSCALL_INT, 0, IDT_TRAP);
-    // idt_add_desc(FORK_INT, 0, IDT_TRAP);
-    // idt_add_desc(EXEC_INT, 0, IDT_TRAP);
-    // idt_add_desc(WAIT_INT, 0, IDT_TRAP);
-    // idt_add_desc(YIELD_INT, 0, IDT_TRAP);
-    // idt_add_desc(DESCHEDULE_INT, 0, IDT_TRAP);
-    // idt_add_desc(MAKE_RUNNABLE_INT, 0, IDT_TRAP);
-    idt_add_desc(GETTID_INT, asm_gettid, IDT_TRAP);
-    idt_add_desc(NEW_PAGES_INT, asm_new_pages, IDT_TRAP);
-    //idt_add_desc(REMOVE_PAGES_INT, remove_pages, IDT_TRAP);
-    // idt_add_desc(SLEEP_INT, 0, IDT_TRAP);
-    // idt_add_desc(GETCHAR_INT, 0, IDT_TRAP);
-    // idt_add_desc(READLINE_INT, 0, IDT_TRAP);
-    // idt_add_desc(PRINT_INT, 0, IDT_TRAP);
-    // idt_add_desc(SET_TERM_COLOR_INT, 0, IDT_TRAP);
-    // idt_add_desc(SET_CURSOR_POS_INT, 0, IDT_TRAP);
-    // idt_add_desc(GET_CURSOR_POS_INT, 0, IDT_TRAP);
-    // idt_add_desc(THREAD_FORK_INT, 0, IDT_TRAP);
-    // idt_add_desc(GET_TICKS_INT, 0, IDT_TRAP);
-    // idt_add_desc(MISBEHAVE_INT, 0, IDT_TRAP);
-    // idt_add_desc(HALT_INT, 0, IDT_TRAP);
-    // idt_add_desc(TASK_VANISH_INT, 0, IDT_TRAP);
-    // idt_add_desc(SET_STATUS_INT, 0, IDT_TRAP);
-    // idt_add_desc(VANISH_INT, 0, IDT_TRAP);
-    // idt_add_desc(READFILE_INT, 0, IDT_TRAP);
-    // idt_add_desc(SWEXN_INT, 0, IDT_TRAP);
+    // idt_add_desc(SYSCALL_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(FORK_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(EXEC_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(WAIT_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(YIELD_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(DESCHEDULE_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(MAKE_RUNNABLE_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    idt_add_desc(GETTID_INT, asm_gettid, IDT_TRAP, IDT_DPL_USER);
+    idt_add_desc(NEW_PAGES_INT, asm_new_pages, IDT_TRAP, IDT_DPL_USER);
+    //idt_add_desc(REMOVE_PAGES_INT, remove_pages, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(SLEEP_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(GETCHAR_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(READLINE_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(PRINT_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(SET_TERM_COLOR_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(SET_CURSOR_POS_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(GET_CURSOR_POS_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(THREAD_FORK_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(GET_TICKS_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(MISBEHAVE_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(HALT_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(TASK_VANISH_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(SET_STATUS_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(VANISH_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(READFILE_INT, 0, IDT_TRAP, IDT_DPL_USER);
+    // idt_add_desc(SWEXN_INT, 0, IDT_TRAP, IDT_DPL_USER);
 }
 
 /** @brief Add a descriptor entry to the IDT.
@@ -62,12 +62,12 @@ void idt_init() {
  *  @param gate_type The descriptor gate type.
  *  @return Void.
  */
-void idt_add_desc(int idt_entry, void *handler, int gate_type)
+void idt_add_desc(int idt_entry, void *handler, unsigned gate_type, unsigned dpl)
 {
     idt_desc_t *idt_desc = (idt_desc_t *)((uint64_t *)idt_base() + idt_entry);
     idt_desc->offset_l = (uint16_t)(int)(handler);
     idt_desc->segment_selector = SEGSEL_KERNEL_CS;
     idt_desc->zeros = 0x00;
-    idt_desc->flags = (gate_type | IDT_D | IDT_DPL | IDT_P);
+    idt_desc->flags = (gate_type | IDT_D | dpl | IDT_P);
     idt_desc->offset_h = (uint16_t)(((int)handler) >> 16);
 }
--- p3/kern/inc/idt.h
+++ p3/kern/inc/idt.h
@@ -17,7 +17,8 @@
 #define IDT_INT 0x06
 #define IDT_TRAP 0x07
 #define IDT_D 0x08
-#define IDT_DPL 0x00
+#define IDT_DPL_KERNEL 0x00
+#define IDT_DPL_USER 0x60
 #define IDT_P 0x80
 
 #define TIMER_IDT_ENTRY 0x20
@@ -34,6 +35,6 @@ typedef struct idt_desc {
 
 void idt_init();
 
-void idt_add_desc(int idt_entry, void *handler, int gate_type);
+void idt_add_desc(int idt_entry, void *handler, unsigned gate_type, unsigned dpl);
 
 #endif /* _IDT_H */
